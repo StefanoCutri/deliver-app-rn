7dd131cd3f83dbaf17df302e3d716a6efd0238bf
Stefano Cutri <stefanocutristecco@gmail.com>
2023-04-25 19:41:29 -0300

    Search bar

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,15 +1,14 @@
-import { View, Text, Image } from "react-native";
-import React from "react";
+import React, { useLayoutEffect } from "react";
+import { View, Text, Image, SafeAreaView } from "react-native";
 import { styled } from "nativewind";
-import { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
-import { SafeAreaView } from "react-native";
-
 import {
+  MagnifyingGlassIcon,
   UserIcon,
   ChevronDownIcon,
-  SearchIcon,
+  AdjustmentsVerticalIcon,
 } from "react-native-heroicons/outline";
+import { TextInput } from "react-native";
 
 const StyledView = styled(View);
 const StyledText = styled(Text);
@@ -25,7 +24,7 @@ const HomeScreen = () => {
   return (
     <SafeAreaView className="bg-white pt-5">
       {/* Header */}
-      <StyledView className="flex-row pb-3 items-center mx-4 space-x-2">
+      <StyledView className="flex-row pb-3 items-center mx-4 space-x-2 px-4">
         <Image
           source={{ uri: "https://links.papareact.com/wru" }}
           className="h-7 w-7 bg-gray-300 p-4 rounded-full"
@@ -40,6 +39,17 @@ const HomeScreen = () => {
           </StyledText>
         </StyledView>
         <UserIcon size={35} color="#00CCBB" />
+      </StyledView>
+      {/* Search */}
+      <StyledView className=" flex-row items-center space-x-2 pb-2 mx-4 px-4">
+        <StyledView className="flex-row space-x-2 bg-gray-200 p-3 flex-1">
+          <MagnifyingGlassIcon color="gray" size={20} />
+          <TextInput
+            placeholder="Restaurants and cuisines"
+            keyboardType="default"
+          />
+        </StyledView>
+        <AdjustmentsVerticalIcon color="#00CCBB" />
       </StyledView>
     </SafeAreaView>
   );
