fdac8b84cc8ad914f8969c97dca9c9948bff40dc
Stefano Cutri <stefanocutristecco@gmail.com>
2023-04-26 16:55:19 -0300

    RestaurantCard styled

--- components/RestaurantCard.js
+++ components/RestaurantCard.js
@@ -1,40 +1,45 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { TouchableOpacity } from 'react-native'
-import { Image } from 'react-native'
-import { StarIcon } from 'react-native-heroicons/outline'
-import {} from 'react-native-heroicons/outline'
+import { View, Text } from "react-native";
+import React from "react";
+import { TouchableOpacity } from "react-native";
+import { Image } from "react-native";
+import { StarIcon } from "react-native-heroicons/outline";
+import { MapIcon } from "react-native-heroicons/outline";
 
 const RestaurantCard = ({
-    id,
-    imgUrl,
-    title,
-    rating,
-    genre,
-    address,
-    short_description,
-    dishes,
-    long,
-    lat
+  id,
+  imgUrl,
+  title,
+  rating,
+  genre,
+  address,
+  short_description,
+  dishes,
+  long,
+  lat,
 }) => {
   return (
-    <TouchableOpacity>
-        <Image
-            source={{
-                uri: imgUrl
-            }}
-            className="h-36 w-64 rounded-sm"
-        />
+    <TouchableOpacity className="bg-white mr-3 shadow">
+      <Image
+        source={{
+          uri: imgUrl,
+        }}
+        className="h-36 w-64 rounded-sm"
+      />
       <View className="px-3 pb-4">
-        <Text className="font-bold text-lg pt-2">
-            {title}
-        </Text>
-        <View>
-
+        <Text className="font-bold text-lg pt-2">{title}</Text>
+        <View className="flex-row items-center space-x-1 ">
+          <StarIcon color="green" opacity={0.5} size={22} />
+          <Text className="text-gray-500 text-xs">
+            <Text className="text-green-500">{rating}</Text> · {genre}
+          </Text>
+        </View>
+        <View className="flex-row items-center space-x-1">
+          <MapIcon color="gray" opacity={0.4} size={22}/>
+          <Text className="text-xs">Nearby · {address}</Text>
         </View>
       </View>
     </TouchableOpacity>
-  )
-}
+  );
+};
 
-export default RestaurantCard
+export default RestaurantCard;
