c4cdc9b1a843b4c9c414c7e7a44e5174db58a7ab
Stefano Cutri <stefanocutristecco@gmail.com>
2023-04-25 21:01:48 -0300

    Categories components styled

--- components/Categories.js
+++ components/Categories.js
@@ -1,14 +1,29 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { ScrollView } from 'react-native'
+import { View, Text } from "react-native";
+import React from "react";
+import { ScrollView } from "react-native";
+import CategoryCard from "./CategoryCard";
 
 const Categories = () => {
   return (
-    <ScrollView>
+    <ScrollView
+      horizontal
+      contentContainerStyle={{
+        paddingHorizontal: 15,
+        paddingTop: 10,
+      }}
+      showsHorizontalScrollIndicator={false}
+    >
       {/* CategoryCard */}
-      <Text>Categories</Text>
+      <CategoryCard imgUrl="https://assets.unileversolutions.com/recipes-v2/237341.jpg" title="Testing"/>
+      <CategoryCard imgUrl="https://assets.unileversolutions.com/recipes-v2/237341.jpg" title="Testing"/>
+      <CategoryCard imgUrl="https://assets.unileversolutions.com/recipes-v2/237341.jpg" title="Testing"/>
+      <CategoryCard imgUrl="https://assets.unileversolutions.com/recipes-v2/237341.jpg" title="Testing"/>
+      <CategoryCard imgUrl="https://assets.unileversolutions.com/recipes-v2/237341.jpg" title="Testing"/>
+      <CategoryCard imgUrl="https://assets.unileversolutions.com/recipes-v2/237341.jpg" title="Testing"/>
+      <CategoryCard imgUrl="https://assets.unileversolutions.com/recipes-v2/237341.jpg" title="Testing"/>
+      <CategoryCard imgUrl="https://assets.unileversolutions.com/recipes-v2/237341.jpg" title="Testing"/>
     </ScrollView>
-  )
-}
+  );
+};
 
-export default Categories
+export default Categories;
