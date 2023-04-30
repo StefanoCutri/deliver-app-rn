984fbe0ced1bf0e8f91232ea0ad9b70ffec6de6b
Stefano Cutri <stefanocutristecco@gmail.com>
2023-04-29 21:14:01 -0300

    Install react-redux and set up the provider

--- App.js
+++ App.js
@@ -4,14 +4,18 @@ import 'react-native-url-polyfill/auto';
 const Stack = createNativeStackNavigator();
 import HomeScreen from "./screens/HomeScreen";
 import RestaurantScreen from "./screens/RestaurantScreen";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 export default function App() {
   return (
     <NavigationContainer>
+      <Provider store={store}>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Restaurant" component={RestaurantScreen} />
       </Stack.Navigator>
+      </Provider>
     </NavigationContainer>
   );
 }
