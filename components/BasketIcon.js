e7c0990a8659fbea32aa9e19406bb5af2ea77865
Stefano Cutri <stefanocutristecco@gmail.com>
2023-05-02 17:54:54 -0300

    BasketIcon styled

--- components/BasketIcon.js
+++ components/BasketIcon.js
@@ -1,5 +1,6 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
+import Currency from "react-currency-formatter";
 import { useSelector } from 'react-redux'
 import { selectBasketItems, selectBasketTotal } from '../features/basketSlice'
 import { useNavigation } from '@react-navigation/native'
@@ -10,8 +11,14 @@ const BasketIcon = () => {
     const basketTotal = useSelector(selectBasketTotal)
 
   return (
-    <View>
-      <Text>BasketIcon</Text>
+    <View className="absolute bottom-10 w-full z-50">
+        <TouchableOpacity className="mx-5 bg-[#00CCBB] p-4 rounded-lg flex-row items-center space-x-1">
+        <Text className="text-lg text-white font-extrabold bg-[#01A296] py-1 px-2">{items.length}</Text>
+        <Text className="flex-1 text-white font-extrabold text-lg text-center">View Basket</Text>
+        <Text className="text-lg text-white font-extrabold">
+            <Currency quantity={basketTotal} currency='USD'/>
+        </Text>
+        </TouchableOpacity>
     </View>
   )
 }
