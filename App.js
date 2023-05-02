02340fe46b01a51e155a969f0e66eb27e619f408
Stefano Cutri <stefanocutristecco@gmail.com>
2023-05-02 18:10:00 -0300

    Restaurant Reducer connected to store

---
+++ App.js
@@ -0,0 +1,27 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import 'react-native-url-polyfill/auto';
+const Stack = createNativeStackNavigator();
+import HomeScreen from "./screens/HomeScreen";
+import RestaurantScreen from "./screens/RestaurantScreen";
+import { Provider } from "react-redux";
+import { store } from "./store";
+import BasketScreen from "./screens/BasketScreen";
+
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Provider store={store}>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Restaurant" component={RestaurantScreen} />
+        <Stack.Screen name="Basket" component={BasketScreen} options={{
+          presentation: "modal",
+          headerShown: false
+        }}/>
+      </Stack.Navigator>
+      </Provider>
+    </NavigationContainer>
+  );
+}
